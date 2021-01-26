b335b4a6848e05de6896f4adf1125ffc8b601d00
Radek <283680@pw.edu.pl>
2021-01-26 09:13:11 +0100

    console mode all working

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import argparse
 import operator
 import shutil
+import subprocess
 from pathlib import Path
 
 import cv2 as cv
@@ -221,9 +223,13 @@ def crop_notes(divider_param, max_notes, train_notes_list, path_in, path_out, ex
 
 def prepare_for_yolo(dict_of_notes, train_ratio, each_note_copies, notes_path, yolo_dataset_path, ext):
     input_path = Path(notes_path)
+    output_path = Path(yolo_dataset_path)
 
     if input_path.is_dir() is not True:
         raise Exception('input_path should exist, bad input')
+    elif output_path.is_dir():
+        print('Dataset already prepared, skipping...')
+        return
 
     if 1 < train_ratio < 0:
         raise Exception('Split ratio not in (0, 1]')
@@ -235,7 +241,6 @@ def prepare_for_yolo(dict_of_notes, train_ratio, each_note_copies, notes_path, y
 
     train_set = ids[:train_notes_count]
 
-    output_path = Path(yolo_dataset_path)
     train_img_path = output_path / 'images' / 'train'
     validate_img_path = output_path / 'images' / 'validate'
     train_labels_path = output_path / 'labels' / 'train'
@@ -283,6 +288,9 @@ def get_song_notes_dict(dict_of_classes, labels_path):
     input_path = Path(labels_path)
     if input_path.is_dir() is not True:
         raise Exception('No detected folder. You should run detection first.')
+    elif not any(input_path.iterdir()):
+        raise Exception('No data in \'detected\' folder. You should improve detection first.')
+
     notes = list()
     song_names = set()
     for file in input_path.iterdir():
@@ -309,12 +317,23 @@ def get_song_notes_dict(dict_of_classes, labels_path):
     return song_notes
 
 
-def get_commands(yolov5_proj_dir, batch_size, confidence, data_config, det_info_path, detect_img_size, epochs,
-                 network_config,
-                 network_name, network_type, train_img_size, trained_net_path, user_staffs_path):
-    train_command = f'cd {yolov5_proj_dir} && python train.py --img {train_img_size} --batch {batch_size} --epochs {epochs} --data {data_config} --cfg {network_config} --weights {network_type}.pt --name {network_name} --cache --device 0'
-    detect_command = f'cd {yolov5_proj_dir} && python detect.py --source .{user_staffs_path} --weights {trained_net_path} --img {detect_img_size} --conf {confidence} --project {det_info_path} --name {network_name} --save-txt --save-conf --device 0'
-    return train_command, detect_command
+def exec_detect_command(confidence, det_info_path, detect_img_size, network_name, trained_net_path, user_staffs_path,
+                        yolov5_proj_dir):
+    if Path(f'{det_info_path}/{network_name}').is_dir():
+        print('Data already detected, skipping...')
+        return
+    cmd = f'python detect.py --source .{user_staffs_path} --weights {trained_net_path} --img {detect_img_size} --conf {confidence} --project .{det_info_path} --name {network_name} --save-txt --save-conf --device 0'
+    return subprocess.call(cmd, shell=True, cwd=f'{yolov5_proj_dir}')
+
+
+def exec_train_command(batch_size, data_config, epochs, network_config, network_name, network_type, train_img_size,
+                       yolov5_proj_dir):
+    if Path(f'{yolov5_proj_dir}/runs/train/{network_name}').is_dir():
+        print('Network already trained, skipping...')
+        return
+    else:
+        cmd = f'python train.py --img {train_img_size} --batch {batch_size} --epochs {epochs} --data {data_config} --cfg {network_config} --weights {network_type}.pt --name {network_name} --cache --device 0'
+        return subprocess.call(cmd, shell=True, cwd=f'{yolov5_proj_dir}')
 
 
 def generate_midi(song_notes, note_numbers, note_values, music_path):
@@ -330,6 +349,10 @@ def generate_midi(song_notes, note_numbers, note_values, music_path):
     tempo = 80  # In BPM
     volume = 100  # 0-127, as per the MIDI standard
     for song_name in song_notes:
+        midi_file = (output_path / f'{song_name}.mid')
+        if midi_file.exists():
+            print(f'Song {song_name} already exist, skipping...')
+            continue
         # degrees = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
         # print(song_name, song_notes[song_name])
         degrees = [(note_values[note.value], note_numbers[note.height]) for note in song_notes[song_name]]
@@ -383,7 +406,12 @@ def generate_midi(song_notes, note_numbers, note_values, music_path):
 #         raise SystemExit
 
 
-def main():
+def main(params):
+    """ Training / detect data parameters preparation """
+    # Raw train / detect photos extension
+    ext = '.jpg'
+    # How many notes of each type in training dataset
+    each_note_copies = 20
     # List of notes included in train data
     train_notes_list = ['c_a', 'c_ais', 'c_b', 'c_h', 'c_c1', 'c_cis1', 'c_des1', 'c_d1', 'c_dis1', 'c_es1', 'c_e1',
                         'c_f1', 'c_fis1', 'c_ges1', 'c_g1', 'c_gis1', 'c_as1', 'c_a1', 'c_ais1', 'c_b1', 'c_h1', 'c_c2',
@@ -400,84 +428,25 @@ def main():
                         'w_f1', 'w_fis1', 'w_ges1', 'w_g1', 'w_gis1', 'w_as1', 'w_a1', 'w_ais1', 'w_b1', 'w_h1', 'w_c2',
                         'w_cis2', 'w_des2', 'w_d2', 'w_dis2', 'w_es2', 'w_e2', 'w_f2', 'w_fis2', 'w_ges2', 'w_g2',
                         'w_gis2', 'w_as2', 'w_a2', 'w_ais2', 'w_b2', 'w_h2', 'w_c3']
-
+    # Helper dictionaries regarding train data
     dict_of_notes = {train_notes_list[i]: i for i in range(0, len(train_notes_list))}
     dict_of_classes = {i: train_notes_list[i] for i in range(0, len(train_notes_list))}
 
-    # Raw photo extension
-    ext = '.jpg'
-
-    """ Crop all training data sheets """
-    # Path with raw train sheets
-    raw_sheets_path = r'./sheets/database/raw'
-    # Path where to store cropped sheets
-    cropped_sheets_path = r'./sheets/database/cropped'
-
-    # crop_all_sheets(raw_sheets_path, cropped_sheets_path, ext)
-
-    """ Preprocess sheets before extracting notes from trainig data sheets """
-    # Path where to store preprocessed sheets
-    preprocessed_sheets_path = r'./sheets/database/preprocessed'
+    """ # Training / detect data parameters preparation """
     # What height to resize the page to
     page_height = 1600
-    # How much to crop the borders
+    # How much to crop the borders of the input photo
     page_margin = 40
-
-    # preprocess_sheets(page_height, page_margin, cropped_sheets_path, preprocessed_sheets_path, ext)
-
-    """ Crop out staffs from preprocessed sheets """
-    # Path where to store the staffs
-    staffs_path = r'./sheets/database/staffs'
     # How much to crop borders of staff image
     staff_margin = 5
-
-    # crop_staffs(staff_margin, preprocessed_sheets_path, staffs_path, ext)
-
-    """ Crop notes by vertical tact lines """
-    # Path where to store individual notes
-    notes_path = r'./sheets/database/notes'
-    # Parameter determining vertical and horizontal filter kernel dimensions
-    height_divider = 33
-    # How many notes of each type in training dataset
-    each_note_copies = 20
-
-    # crop_notes(height_divider, each_note_copies, train_notes_list, staffs_path, notes_path, ext)
-
-    """ Generate yolo compatible dataset """
-    # Path where to store yolo-like database
-    yolo_dataset_path = r'./yolo_notes'
-    # How to split files for training / validation
-    training_ratio = 0.5
-
-    # prepare_for_yolo(dict_of_notes, training_ratio, each_note_copies, notes_path, yolo_dataset_path, ext)
-
-    """ Preprocess user input """
-    # Path with raw user sheets
-    user_raw_path = r'./sheets/user/raw'
-    # Path where to store cropped user sheets
-    user_cropped_path = r'./sheets/user/cropped'
-
-    # crop_all_sheets(user_raw_path, user_cropped_path, ext)
-
-    """ Preprocess user sheets before extracting staffs from user data sheets """
-    # Path where to store preprocessed sheets
-    user_preprocessed_path = r'./sheets/user/preprocessed'
-
-    # preprocess_sheets(page_height, page_margin, user_cropped_path, user_preprocessed_path, ext)
-
-    """ Crop out staffs from preprocessed sheets """
-    # Path where to store the staffs
-    user_staffs_path = r'./sheets/user/staffs'
-
-    # crop_staffs(staff_margin, user_preprocessed_path, user_staffs_path, ext)
 
     """ Prepare parameters for YOLOv5 commands """
     yolov5_proj_dir = r'./yolov5'
     network_type = 'yolov5x'
     project_name = 'notes'
     train_img_size = 96
-    batch_size = 256
-    epochs = 500
+    batch_size = 256  # 256
+    epochs = 300  # 200
     data_config = r'../config/moje.yaml'
     network_config = f'../config/{network_type}.yaml'
     network_name = f'{network_type}_{project_name}_s{train_img_size}_b{batch_size}_e{epochs}'
@@ -485,84 +454,139 @@ def main():
     trained_net_path = f'./runs/train/{network_name}/weights/best.pt'
     detect_img_size = 960
     confidence = 0.5
-    det_info_path = '../detected'
+    det_info_path = './detected'
 
-    """ Generate yolo train and detect command """
-    train_command, detect_command = get_commands(yolov5_proj_dir, batch_size, confidence, data_config, det_info_path,
-                                                 detect_img_size, epochs, network_config, network_name, network_type,
-                                                 train_img_size, trained_net_path, user_staffs_path)
+    if params.mode == 'admin':
+        # Path where to store individual notes
+        notes_path = r'./sheets/database/notes'
+        if Path(notes_path).is_dir() is not True:
+            """ Crop all training data sheets """
+            # Path with raw train sheets
+            raw_sheets_path = r'./sheets/database/raw'
+            # Path where to store cropped sheets
+            cropped_sheets_path = r'./sheets/database/cropped'
 
-    """ Train network on the prepared dataset """
-    # exec(train_command)
-    # print(train_command)
+            crop_all_sheets(raw_sheets_path, cropped_sheets_path, ext)
 
-    """ Detect notes on user input using trained network """
-    # exec(detect_command)
-    print(detect_command)
+            """ Preprocess sheets before extracting notes from trainig data sheets """
+            # Path where to store preprocessed sheets
+            preprocessed_sheets_path = r'./sheets/database/preprocessed'
 
-    """ Extract notes information after detection """
-    # Path where the information is stored
-    labels_path = f'./detected/{network_name}/labels'
-    # Gets {song_name: notes_list} dictionary
-    song_notes = get_song_notes_dict(dict_of_classes, labels_path)
+            preprocess_sheets(page_height, page_margin, cropped_sheets_path, preprocessed_sheets_path, ext)
 
-    """ Prepare notes parameters """
-    # Translates note names to MIDI values
-    note_numbers = {
-        'a': 57,
-        'ais': 58,
-        'b': 58,
-        'h': 59,
-        'c1': 60,
-        'cis1': 61,
-        'des1': 61,
-        'd1': 62,
-        'dis1': 63,
-        'es1': 63,
-        'e1': 64,
-        'f1': 65,
-        'fis1': 66,
-        'ges1': 66,
-        'g1': 67,
-        'gis1': 68,
-        'as1': 68,
-        'a1': 69,
-        'ais1': 70,
-        'b1': 70,
-        'h1': 71,
-        'c2': 72,
-        'cis2': 73,
-        'des2': 73,
-        'd2': 74,
-        'dis2': 75,
-        'es2': 75,
-        'e2': 76,
-        'f2': 77,
-        'fis2': 78,
-        'ges2': 78,
-        'g2': 79,
-        'gis2': 80,
-        'as2': 80,
-        'a2': 81,
-        'ais2': 82,
-        'b2': 82,
-        'h2': 83,
-        'c3': 84
-    }
-    # Translates note prefix to MIDI height parameter
-    note_values = {
-        'c': 4.0,
-        'o': 0.5,
-        'p': 2.0,
-        'w': 1.0
-    }
+            """ Crop out staffs from preprocessed sheets """
+            # Path where to store the staffs
+            staffs_path = r'./sheets/database/staffs'
 
-    """ Generate MIDI file from notes """
-    music_path = r'./music'
+            crop_staffs(staff_margin, preprocessed_sheets_path, staffs_path, ext)
 
-    generate_midi(song_notes, note_numbers, note_values, music_path)
+            """ Crop notes by vertical tact lines """
+            # Parameter determining vertical and horizontal filter kernel dimensions
+            height_divider = 33
 
-    # play_midi()
+            crop_notes(height_divider, each_note_copies, train_notes_list, staffs_path, notes_path, ext)
+
+        """ Generate yolo compatible dataset """
+        # Path where to store yolo-like database
+        yolo_dataset_path = r'./yolo_notes'
+        # How to split files for training / validation
+        training_ratio = 0.65
+
+        prepare_for_yolo(dict_of_notes, training_ratio, each_note_copies, notes_path, yolo_dataset_path, ext)
+
+        """ Train network on the prepared dataset """
+        exec_train_command(batch_size, data_config, epochs, network_config, network_name, network_type, train_img_size,
+                           yolov5_proj_dir)
+
+    # Path where to store the user staffs
+    user_staffs_path = r'./sheets/user/staffs'
+
+    if not Path(user_staffs_path).is_dir():
+        """ Preprocess user input """
+        # Path with raw user sheets
+        user_raw_path = r'./sheets/user/raw'
+        # Path where to store cropped user sheets
+        user_cropped_path = r'./sheets/user/cropped'
+
+        crop_all_sheets(user_raw_path, user_cropped_path, ext)
+
+        """ Preprocess user sheets before extracting staffs from user data sheets """
+        # Path where to store preprocessed sheets
+        user_preprocessed_path = r'./sheets/user/preprocessed'
+
+        preprocess_sheets(page_height, page_margin, user_cropped_path, user_preprocessed_path, ext)
+
+        """ Crop out staffs from preprocessed sheets """
+        crop_staffs(staff_margin, user_preprocessed_path, user_staffs_path, ext)
+    else:
+        print('User data already prepared, skipping...')
+
+    if params.generate:
+        """ Detect notes on user input using trained network """
+        exec_detect_command(confidence, det_info_path, detect_img_size, network_name, trained_net_path,
+                            user_staffs_path, yolov5_proj_dir)
+
+        """ Extract notes information after detection """
+        # Path where the information is stored
+        labels_path = f'./detected/{network_name}/labels'
+        # Gets {song_name: notes_list} dictionary
+        song_notes = get_song_notes_dict(dict_of_classes, labels_path)
+
+        """ Prepare notes parameters """
+        # Translates note names to MIDI values
+        note_numbers = {
+            'a': 57,
+            'ais': 58,
+            'b': 58,
+            'h': 59,
+            'c1': 60,
+            'cis1': 61,
+            'des1': 61,
+            'd1': 62,
+            'dis1': 63,
+            'es1': 63,
+            'e1': 64,
+            'f1': 65,
+            'fis1': 66,
+            'ges1': 66,
+            'g1': 67,
+            'gis1': 68,
+            'as1': 68,
+            'a1': 69,
+            'ais1': 70,
+            'b1': 70,
+            'h1': 71,
+            'c2': 72,
+            'cis2': 73,
+            'des2': 73,
+            'd2': 74,
+            'dis2': 75,
+            'es2': 75,
+            'e2': 76,
+            'f2': 77,
+            'fis2': 78,
+            'ges2': 78,
+            'g2': 79,
+            'gis2': 80,
+            'as2': 80,
+            'a2': 81,
+            'ais2': 82,
+            'b2': 82,
+            'h2': 83,
+            'c3': 84
+        }
+        # Translates note prefix to MIDI height parameter
+        note_values = {
+            'c': 4.0,
+            'o': 0.5,
+            'p': 2.0,
+            'w': 1.0
+        }
+
+        """ Generate MIDI file from notes """
+        music_path = r'./music'
+
+        generate_midi(song_notes, note_numbers, note_values, music_path)
 
 
 class NoteDetails:
@@ -582,4 +606,8 @@ class NoteDetails:
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='user', help='Tryb aplikacji user / admin')
+    parser.add_argument('--generate', action='store_true', help='Detects & generates music')
+    opt = parser.parse_args(f'--mode admin --generate'.split())
+    main(opt)
